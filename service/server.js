629304010096469b9ee8d1f30c4ba575ee3a75e9
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-06-14 11:12:08 -0700

    Fix output of service function to match proto response, set up request handler

--- service/server.js
+++ service/server.js
@@ -14,7 +14,7 @@ const packageDefinition = protoLoader.loadSync(
 //cipher_proto variable points to cipher package, so it now has all the proto definitions
 const cipher_proto = grpc.loadPackageDefinition(packageDefinition).cipher;
 
-
+//Implement the service interface generated from the service definition
 //Encode
 const encode = (request) => {
     console.log(`Encode called with request: ${JSON.stringify(request)}`)
@@ -32,8 +32,8 @@ const encode = (request) => {
             ciphered.push(getNewLetter(letter, newKey, alpha));
         }  
     }
-    console.log('About to return', ciphered.join(''));
-    return ciphered.join('');
+    //return must match proto definition Message = {msg: string} 
+    return {msg: ciphered.join('')};
 }
 
 const getNewLetter = (letter, key, alpha) => {
@@ -41,10 +41,11 @@ const getNewLetter = (letter, key, alpha) => {
     return newLetterIdx <= 25 ? alpha[newLetterIdx] : alpha[-1 + (newLetterIdx % 25)];
 }
 
-function encodeMessage(call, callback) {
-    // console.log("FROM ENCODE MSG CALL", encode(call.request));
-    callback(null, encode(call.request));
-}
+//this func is passed a call obj for the RPC, which has the Input param as a property, and a cb to which we pass our returned Message
+// function encodeMessage(call, callback) {
+//     console.log("Encode call outputting:", encode(call.request));
+//     callback(null, encode(call.request)); //first null indicates no error
+// }
 
 
 //Decode
@@ -53,23 +54,32 @@ const decode = (request) => {
     return encode(request);
 }
 
-function decodeMessage(call, callback) {
-    callback(null, decode(call.request));
+// function decodeMessage(call, callback) {
+//     callback(null, decode(call.request));
+// }
+
+//Handle Request
+const handleRequest = (call, callback) => {
+    if (call.request.encode) {
+        callback(null, encode(call.request)); 
+    } else {
+        callback(null, decode(call.request));
+    }
 }
 
 
-//Build server
+//Run the gRPC server
 const getServer = () => {
     const server = new grpc.Server();
-    server.addService(cipher_proto.CipherService.service, {
-        encodeMessage: encodeMessage,
-        decodeMessage: decodeMessage
+    server.addService(cipher_proto.CipherService.service, { //uses svc descriptor, which is a property of the stub
+        encodeMessage: handleRequest, //implement svc methods
+        decodeMessage: handleRequest
     });
     return server;
 }
 
-let routeServer = getServer();
-routeServer.bind('0.0.0.0:50051',
+let routeServer = getServer(); //create server instance
+routeServer.bind('0.0.0.0:50051', //specify address/port
     grpc.ServerCredentials.createInsecure());
 console.log('Server listening on Port 50051');
 routeServer.start();
